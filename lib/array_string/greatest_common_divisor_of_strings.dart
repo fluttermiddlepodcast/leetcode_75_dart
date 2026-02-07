707d621e2f785ce6371c6febff46672c8d295f4a
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2026-02-07 08:42:35 +0300

    added "1071. Greatest Common Divisor of Strings"

---
+++ lib/array_string/greatest_common_divisor_of_strings.dart
@@ -0,0 +1,15 @@
+class GreatestCommonDivisorOfStrings {
+  String gcdOfStrings(String str1, String str2) {
+    if (str1 + str2 != str2 + str1) {
+      return '';
+    }
+
+    final endIndex = _gcd(str1.length, str2.length);
+
+    return str1.substring(0, endIndex);
+  }
+
+  int _gcd(int a, int b) {
+    return b == 0 ? a : _gcd(b, a % b);
+  }
+}
