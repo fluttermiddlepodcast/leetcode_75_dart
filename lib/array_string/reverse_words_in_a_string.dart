4c6c5eb2c782f51f857ddc62d4412a53fd29e7b8
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2026-02-12 06:53:19 +0300

    added "151. Reverse Words in a String"

---
+++ lib/array_string/reverse_words_in_a_string.dart
@@ -0,0 +1,43 @@
+class ReverseWordsInAString {
+  String reverseWords(String s) {
+    final words = [];
+    var i = 0;
+    var n = s.length;
+
+    while (i < n) {
+      while (i < n && s[i] == ' ') {
+        i++;
+      }
+
+      if (i >= n) {
+        break;
+      }
+
+      var start = i;
+      while (i < n && s[i] != ' ') {
+        i++;
+      }
+
+      words.add(s.substring(start, i));
+    }
+
+    var left = 0;
+    var right = words.length - 1;
+
+    while (left < right) {
+      final temp = words[left];
+      words[left] = words[right];
+      words[right] = temp;
+      left++;
+      right--;
+    }
+
+    final result = StringBuffer();
+    for (var i = 0; i < words.length; i++) {
+      if (i > 0) result.write(' ');
+      result.write(words[i]);
+    }
+
+    return result.toString();
+  }
+}
