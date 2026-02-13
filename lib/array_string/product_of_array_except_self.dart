4a91a219c6805013fe8bb2d0481afc9af6a20fce
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2026-02-13 06:34:45 +0300

    added "238. Product of Array Except Self"

---
+++ lib/array_string/product_of_array_except_self.dart
@@ -0,0 +1,18 @@
+class ProductOfArrayExceptSelf {
+  List<int> productExceptSelf(List<int> nums) {
+    final n = nums.length;
+    final result = List.filled(n, 1);
+
+    for (var i = 1; i < n; i++) {
+      result[i] = result[i - 1] * nums[i - 1];
+    }
+
+    var product = 1;
+    for (var i = n - 1; i >= 0; i--) {
+      result[i] *= product;
+      product *= nums[i];
+    }
+
+    return result;
+  }
+}
