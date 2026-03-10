a71423aa044f010651da64fa67773bf518a47222
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2026-03-10 07:34:49 +0300

    added "2130. Maximum Twin Sum of a Linked List"

---
+++ test/linked_list/maximum_twin_sum_of_a_linked_list_test.dart
@@ -0,0 +1,49 @@
+import 'package:leetcode_dart/linked_list/maximum_twin_sum_of_a_linked_list.dart';
+import 'package:test/test.dart';
+
+import '../common/linked_list.dart';
+
+void main() {
+  group(
+    'Example tests',
+    () {
+      final solution = MaximumTwinSumOfALinkedList();
+
+      test(
+        '6',
+        () => expect(
+          6,
+          solution.pairSum(
+            linkedListFromList(
+              [5, 4, 2, 1],
+            ),
+          ),
+        ),
+      );
+
+      test(
+        '7',
+        () => expect(
+          7,
+          solution.pairSum(
+            linkedListFromList(
+              [4, 2, 2, 3],
+            ),
+          ),
+        ),
+      );
+
+      test(
+        '100001',
+        () => expect(
+          100001,
+          solution.pairSum(
+            linkedListFromList(
+              [1, 100000],
+            ),
+          ),
+        ),
+      );
+    },
+  );
+}
