1c1b2da77dacc99c62299fa4f7660bb58a1ceca1
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2026-03-10 06:49:23 +0300

    added "328. Odd Even Linked List"

---
+++ test/linked_list/odd_even_linked_list_test.dart
@@ -0,0 +1,47 @@
+import 'package:leetcode_dart/linked_list/odd_even_linked_list.dart';
+import 'package:test/test.dart';
+
+import '../common/linked_list.dart';
+
+void main() {
+  group(
+    'Example tests',
+    () {
+      final solution = OddEvenLinkedList();
+
+      test(
+        '[1, 3, 5, 2, 4]',
+        () => expect(
+          true,
+          compareLinkedLists(
+            linkedListFromList(
+              [1, 3, 5, 2, 4],
+            ),
+            solution.oddEvenList(
+              linkedListFromList(
+                [1, 2, 3, 4, 5],
+              ),
+            ),
+          ),
+        ),
+      );
+
+      test(
+        '[2, 3, 6, 7, 1, 5, 4]',
+        () => expect(
+          true,
+          compareLinkedLists(
+            linkedListFromList(
+              [2, 3, 6, 7, 1, 5, 4],
+            ),
+            solution.oddEvenList(
+              linkedListFromList(
+                [2, 1, 3, 5, 6, 4, 7],
+              ),
+            ),
+          ),
+        ),
+      );
+    },
+  );
+}
