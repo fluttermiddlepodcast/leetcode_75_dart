5bf4f4cd3955238b3f0a1df27454d850db27fadb
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2026-04-27 07:30:41 +0300

    added "62. Unique Paths"

---
+++ lib/dp_multidimensional/unique_paths.dart
@@ -0,0 +1,17 @@
+class UniquePaths {
+  int uniquePaths(int m, int n) {
+    if (m == 1 || n == 1) {
+      return 1;
+    }
+
+    final dp = List<int>.filled(n, 1);
+
+    for (var i = 1; i < m; i++) {
+      for (var j = 1; j < n; j++) {
+        dp[j] = dp[j] + dp[j - 1];
+      }
+    }
+
+    return dp[n - 1];
+  }
+}
